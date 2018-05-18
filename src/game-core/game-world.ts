1efc1a429bb820f5b0ebfda39e3f852cd113d194
NB90525 <nb90525@navercorp.com>
2018-05-18 13:00:05 +0900

    game-world 리펙토링

--- src/game-core/game-world.ts
+++ src/game-core/game-world.ts
@@ -9,16 +9,10 @@ import { Engine, World, Bodies, Events } from 'matter-js'
 import PhysicalBody from './physical-body';
 import InputManager from './input-manager';
 
+const STAGE_WIDTH:number = 300;
+const STAGE_HEIGHT:number = 600;
+
 export default class GameWorld{
-
-  private static BASE_WIDTH:number = 300;
-  private static BASE_HEIGHT:number = 600;
-
-  // private static RESOLUTION_WIDTH:number = 900;
-  // private static RESOLUTION_HEIGHT:number = 1800;
-
-  // private static STAGE_RATIO_WIDTH = GameWorld.RESOLUTION_WIDTH / GameWorld.BASE_WIDTH;
-  // private static STAGE_RATIO_HEIGHT = GameWorld.RESOLUTION_HEIGHT / GameWorld.BASE_HEIGHT;
 
   private _world:World;
   private _app:PIXI.Application;
@@ -26,15 +20,10 @@ export default class GameWorld{
   private _user:User;
   private _mountain:Mountain;
   private _bodies:PhysicalBody[] = [];
-
-  private _gameLayer:PIXI.DisplayObject;
-  private _eventLayer:PIXI.DisplayObject;
-  private _bgLayer:PIXI.DisplayObject;
-
-
+  private _inputManager:InputManager;
 
   public static GET_STAGE_SIZE():{ width:number, height:number }{
-    return { width:GameWorld.BASE_WIDTH, height:GameWorld.BASE_HEIGHT };
+    return { width:STAGE_WIDTH, height:STAGE_HEIGHT };
   }
 
   public get app():PIXI.Application{
@@ -50,52 +39,55 @@ export default class GameWorld{
   }
 
   constructor( { container } ){
+    this._initWorld();
+    this._initStage( container );
+    this._initInputManager();
+    this._initMountain();
+    this._initUser();
+  }
+
+  private _initWorld():void{
     this._worldEngine = Engine.create();
     this._worldEngine.enableSleeping = true;
     this._world = this._worldEngine.world;
-    this._app = new PIXI.Application( GameWorld.BASE_WIDTH, GameWorld.BASE_HEIGHT, { antialias:false, backgroundColor:0xffffff, resolution:2 } );
+    Engine.run( this._worldEngine );
+  }
+
+  private _initStage( container ):void{
+    this._app = new PIXI.Application( STAGE_WIDTH, STAGE_HEIGHT, { antialias:false, backgroundColor:0xffffff, resolution:2 } );
     const style:CSSStyleDeclaration = this._app.view.style;
-    // this.stage.scale.x = GameWorld.STAGE_RATIO_WIDTH;
-    // this.stage.scale.y = GameWorld.STAGE_RATIO_HEIGHT;
     style.width = style.height = '100%';
     container.appendChild( this._app.view );
-    this.stage.width = GameWorld.BASE_WIDTH;
-    this.stage.height = GameWorld.BASE_HEIGHT;
+    this.stage.width = STAGE_WIDTH;
+    this.stage.height = STAGE_HEIGHT;
+    this.app.ticker.add( this._update.bind( this ) ); 
+  }
 
-    new InputManager( { stage:this.stage, rectangle:{ x:0, y:0, width:GameWorld.BASE_WIDTH, height:GameWorld.BASE_HEIGHT } } );
-  
+  private _initInputManager():void{
+    this._inputManager = new InputManager( { stage:this.stage, rectangle:{ x:0, y:0, width:STAGE_WIDTH, height:STAGE_HEIGHT } } );
+  } 
 
-    this._mountain = new Mountain( this._world, true );
-    this._mountain.leftTopX = 10;
-    this._mountain.leftTopY = GameWorld.BASE_HEIGHT - this._mountain.height - 100;
-    this.addBody( this._mountain );
-
+  private _initUser():void{
     this._user = new User( this._world );
     this._user.x = this._mountain.x; 
     this._user.y = 100;
-    this.addBody( this._user );
-
-    Engine.run( this._worldEngine );
-    this.app.ticker.add( this.update.bind( this ) ); 
-
-    /*
-    Events.on( this._worldEngine, 'collisionStart', (event) => {
-      const pairs = event.pairs;
-      for( let i=0, count=pairs.length ; i<count ; i+=1 ){
-        const p = pairs[i];
-        const bodyA = p.bodyA;
-        const bodyB = p.bodyB;
-      } 
-    });
-    */
+    this._addBody( this._user );
   }
 
-  addBody( body:PhysicalBody ):void{
+  private _initMountain():void{
+    this._mountain = new Mountain( this._world, true );
+    this._mountain.leftTopX = 10;
+    this._mountain.leftTopY = STAGE_HEIGHT - this._mountain.height - 100;
+    this._addBody( this._mountain );
+  }
+
+
+  private _addBody( body:PhysicalBody ):void{
     this._bodies.push( body );
     this.stage.addChild( body.sprite );
   }
 
-  update( delta:number ):void{ 
+  private _update( delta:number ):void{ 
     for( let i=0, count=this._bodies.length ; i<count ; i+=1 ){
       this._bodies[ i ].update();
     }
